dc2f12840742bb59b78d34dc844a2ab8c14c4d2c
Hoonhee Jang <78456934+waguu07@users.noreply.github.com>
2024-06-13 22:51:12 +0900

    Update change_profile.php

--- change_profile.php
+++ change_profile.php
@@ -15,14 +15,14 @@ $result = mysqli_query($db, $query) or die(mysqli_error($db));
 if ($result->num_rows > 0) {
     while($row = mysqli_fetch_array($result)) {
         if($row['user_pw'] == $current_pw){
-            $update = "UPDATE login_info SET user_pw = '$new_pw' WHERE user_id = '$username'";
+            $update = "UPDATE login_info SET user_pw = '$new_pw' WHERE user_id = '$username'"; /*php mysql update W3School*/
             mysqli_query($db, $update) or die(mysqli_error($db));
             $_SESSION['change_success'] = 2;  
         }else{
             $_SESSION['change_success'] = 1;
         }
         if($email !=""){
-            $update = "UPDATE login_info SET user_email = '$email' WHERE user_id = '$username'";
+            $update = "UPDATE login_info SET user_email = '$email' WHERE user_id = '$username'";/*php mysql update W3School*/
             mysqli_query($db, $update) or die(mysqli_error($db));  
             $_SESSION['change_success'] = 2;
         }
